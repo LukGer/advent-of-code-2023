be78d769eb3bb00d3c8bb757f41318b93fa873f7
Lukas Gerhold <lukger1999@gmail.com>
2023-12-19 19:43:32 +0100

    day 7

--- 03/a.ts
+++ 03/a.ts
@@ -2,21 +2,57 @@ const text = await Deno.readTextFile('input_a.txt');
 const lines = text.split('\n');
 
 let sum = 0;
-let match;
 
-const regexp = /([^\d.\n])/gm;
+for (let y = 0; y < lines.length; y++) {
+    const line = lines[y];
 
-const len = lines[0].length;
+    let digit = "";
 
-while ((match = regexp.exec(text)) != null) {
+    for (let x = 0; x < line.length; x++) {
+        const char = line[x];
 
-    const index = match.index;
-    const symbol = match[1];
+        if (isDigit(char)) {
+            digit += char;
+        } else {
+            if (digit !== "") {
+                for (let x_offset = -1 * digit.length; x_offset <= 1; x_offset++) {
+                    for (const y_offset of [-1, 0, 1]) {
+                        const newX = x + x_offset;
+                        const newY = y + y_offset;
 
-    const found_x = index % len;
-    const found_y = Math.floor(index / len);
+                        if (newX < 0 || newX >= lines.length || newY < 0 || newY >= line.length) {
+                            continue;
+                        }
 
+                        const neighbour = lines[newY]?.[newX];
 
+                        console.log("Checking neighbors of ", digit, " at ", newX + 1, newY + 1, " -> ", neighbour, x_offset, y_offset, x, y)
+
+                        if (isSymbol(neighbour)) {
+                            console.log("Found symbol ", neighbour, " at ", newX, newY);
+                            sum += parseInt(digit);
+
+                            x_offset = digit.length;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            digit = "";
+
+        }
+    }
+}
+
+console.log(sum);
+
+function isSymbol(char: string): boolean {
+    return char !== "." && !isDigit(char);
+}
+
+function isDigit(char: string): boolean {
+    return char === "0" || char === "1" || char === "2" || char === "3" || char === "4" || char === "5" || char === "6" || char === "7" || char === "8" || char === "9";
 }
 
 console.log(sum);
---
+++ 07/b.ts
@@ -0,0 +1,140 @@
+const text = await Deno.readTextFile("input.txt");
+const lines = text.split("\n");
+
+let flag = 0;
+
+//#region Parsing
+
+const parsedHands: { hand: string, value: number, bid: number }[] = [];
+
+for (const line of lines) {
+    const [hand, bid] = line.split(" ");
+
+    if (!hand || !bid) {
+        continue;
+    }
+
+    const handValue = getHandValue(hand);
+
+    parsedHands.push({
+        hand: hand,
+        value: handValue.value,
+        bid: parseInt(bid)
+    });
+}
+
+function getHandValue(hand: string) {
+    const cards = hand.split("");
+
+    const seenCardCounts: { card: string, count: number }[] = [];
+
+    let countOfJacks = 0;
+
+    for (const card of cards) {
+        if (card === "J") {
+            countOfJacks++;
+            continue;
+        }
+
+        const seenCard = seenCardCounts.find((x) => x.card === card);
+
+        if (seenCard) {
+            seenCard.count++;
+        } else {
+            seenCardCounts.push({
+                card,
+                count: 1
+            });
+        }
+    }
+
+    seenCardCounts.sort((a, b) => b.count - a.count);
+
+    for (let i = 0; i < countOfJacks; i++) {
+        const highest = seenCardCounts[0];
+
+        if (!highest) {
+            break;
+        }
+
+        highest.count++;
+    }
+
+    if (seenCardCounts.every((x) => x.count === 5)) {
+        return { hand, value: 7 };
+    }
+
+    if (seenCardCounts.some((x) => x.count === 4)) {
+        return { hand, value: 6 };
+    }
+
+    if (seenCardCounts.some((x) => x.count === 3) && seenCardCounts.some((x) => x.count === 2)) {
+        return { hand, value: 5 };
+    }
+
+    if (seenCardCounts.some((x) => x.count === 3)) {
+        return { hand, value: 4 };
+    }
+
+    if (seenCardCounts.filter((x) => x.count === 2).length === 2) {
+        return { hand, value: 3 };
+    }
+
+    if (seenCardCounts.some((x) => x.count === 2)) {
+        return { hand, value: 2 };
+    }
+
+    return { hand, value: 1 };
+}
+
+function getCardValue(card: string) {
+    switch (card) {
+        case "A":
+            return 14;
+        case "K":
+            return 13;
+        case "Q":
+            return 12;
+        case "T":
+            return 11;
+        case "J":
+            return 1;
+        default:
+            return parseInt(card) + 1;
+    }
+
+}
+
+
+//#endregion
+
+//#region Calculating
+
+const sortedHands = parsedHands.sort((a, b) => {
+    if (a.value === b.value) {
+        for (let i = 0; i < a.hand.length; i++) {
+            const aCard = a.hand[i];
+            const bCard = b.hand[i];
+
+            if (aCard !== bCard) {
+                return getCardValue(aCard) - getCardValue(bCard);
+            }
+        }
+
+        return 0;
+    }
+
+    return a.value - b.value;
+});
+
+for (let i = 0; i < sortedHands.length; i++) {
+    const currentHand = sortedHands[i];
+
+    const valuation = currentHand.bid * (i + 1);
+
+    flag += valuation;
+}
+
+//#endregion
+
+console.log(flag);
